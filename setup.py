20be6e8f7be372f2f5d65ac06ee06f1a40860155
vlazarenko <vlazarenko@squeeze.local>
2011-10-11 15:31:01 +0200

    Lowered needed paramiko to 1.7.6, due to squeeze stupidity

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'fudge'],
-    install_requires=['paramiko>=1.7.7.1'],
+    install_requires=['paramiko>=1.7.6'],
     entry_points={
         'console_scripts': [
             'fab = fabric.main:main',
